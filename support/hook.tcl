b9a3a8b08ae3c6d74d17ca8210e401f6bab33cdf
Rahul Kunwar <rkunwark@xilinx.com>
2017-09-12 14:41:48 +0530

    Support now works with tcl hook script objects

--- support/hook.tcl
+++ support/hook.tcl
@@ -1,10 +1,16 @@
 namespace eval ::xhub::board::support {}
 
-proc ::xhub::board::support::install {} {
+proc ::xhub::board::support::install {xitem xstore} {
+    set item_name [get_property NAME xitem]
+    set store_name [get_property NAME xstore]
+    puts "$item_name item of $store_name is installed"
     puts "Test call to install proc !"
 }
 
-proc ::xhub::board::support::uninstall {} {
+proc ::xhub::board::support::uninstall {xitem xstore} {
+    set item_name [get_property NAME xitem]
+    set store_name [get_property NAME xstore]
+    puts "$item_name item of $store_name is uninstalled"
     puts "Test call to uninstall proc !"
 }
 
