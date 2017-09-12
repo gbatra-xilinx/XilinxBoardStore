b0f0e9201e842d9638bed06124afdc3df65bcc41
Rahul Kunwar <rkunwark@xilinx.com>
2017-09-13 01:54:04 +0530

    Update hook.tcl

--- support/hook.tcl
+++ support/hook.tcl
@@ -1,15 +1,15 @@
 namespace eval ::xhub::board::support {}
 
 proc ::xhub::board::support::install {xitem xstore} {
-    set item_name [get_property NAME xitem]
-    set store_name [get_property NAME xstore]
+    set item_name [get_property NAME $xitem]
+    set store_name [get_property NAME $xstore]
     puts "$item_name item of $store_name is installed"
     puts "Test call to install proc !"
 }
 
 proc ::xhub::board::support::uninstall {xitem xstore} {
-    set item_name [get_property NAME xitem]
-    set store_name [get_property NAME xstore]
+    set item_name [get_property NAME $xitem]
+    set store_name [get_property NAME $xstore]
     puts "$item_name item of $store_name is uninstalled"
     puts "Test call to uninstall proc !"
 }
